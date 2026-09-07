65ec326ca5ad5d2a4a55c401513d718fc02c9a56
izzatulnazifa04 <izzatulnazifa04@gmail.com>
2026-09-07 21:53:39 +0800

    create project

---
+++ GymMembership/src/gymmembership/GymMembership.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package gymmembership;
+
+/**
+ *
+ * @author IZZATUL NAZIFA
+ */
+public class GymMembership {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
