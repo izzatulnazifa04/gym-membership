f696e8477b33e39a965d983988fe64cd369f5ad6
izzatulnazifa04 <izzatulnazifa04@gmail.com>
2026-09-08 18:30:18 +0800

    Fix main method declaration in GymMembership.java

--- GymMembership/src/gymmembership/GymMembership.java
+++ GymMembership/src/gymmembership/GymMembership.java
@@ -6,7 +6,7 @@ public class GymMembership {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        public
     }
     
 }
