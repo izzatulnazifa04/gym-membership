0379bd32e1a8aa4edca45fb882cec7657d2f74ad
izzatulnazifa04 <izzatulnazifa04@gmail.com>
2026-09-08 19:42:20 +0800

    Refactor package declaration and remove unused import statements in GymMembership.java

--- GymMembership/src/gymmembership/GymMembership.java
+++ GymMembership/src/gymmembership/GymMembership.java
@@ -1,27 +1,12 @@
-package gym.ui;
+package gymmembership;
 
-import gym.data.MembershipRepository;
-import gym.model.Membership;
-import gym.model.MembershipManager;
-import gym.model.MonthlyMembership;
-import gym.model.YearlyMembership;
-import java.awt.Dimension;
-import java.awt.FlowLayout;
-import java.awt.Font;
-import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
-import java.awt.GridLayout;
-import java.awt.Insets;
-import java.sql.SQLException;
-import java.time.LocalDate; // test
-
-public class GymMembership { /*test*/
+public class GymMembership {
     private String name;
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        public
+        // TODO code application logic here
     }
     
 }
