0c900bb301f6653cc41f309836119e20332b6855
izzatulnazifa04 <izzatulnazifa04@gmail.com>
2026-09-08 18:32:59 +0800

    Refactor package declaration and import statements in GymMembership.java

--- GymMembership/src/gymmembership/GymMembership.java
+++ GymMembership/src/gymmembership/GymMembership.java
@@ -1,4 +1,20 @@
-package gymmembership;
+package gym.ui;
+
+import gym.data.MembershipRepository;
+import gym.model.Membership;
+import gym.model.MembershipManager;
+import gym.model.MonthlyMembership;
+import gym.model.YearlyMembership;
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.GridLayout;
+import java.awt.Insets;
+import java.sql.SQLException;
+import java.time.LocalDate;
 
 public class GymMembership {
     private String name;
